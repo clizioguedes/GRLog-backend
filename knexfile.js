1d89504fc9afa0a3b575d264a67a7c89d69a31ff
Clizio Guedes <clizioguedes@gmail.com>
2020-08-30 18:48:15 -0300

    Configurando knex.js para conectar com banco remoto em produção

--- knexfile.js
+++ knexfile.js
@@ -34,10 +34,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      database: "orcamentos",
-      user: "postgres",
-      password: "root",
-      host: "127.0.0.1",
+      database: "ddqkgms0i94bm",
+      user: "nlkqgvqpajsydh",
+      password: "bd3b70474dd68dbb3a3bb4131983f331aefe882f11568f1e741793f1fce94967",
+      host: "ec2-107-20-15-85.compute-1.amazonaws.com",
     },
     pool: {
       min: 2,
